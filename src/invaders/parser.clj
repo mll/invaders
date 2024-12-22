41fc8177e0228ceb9ef2417789578a51912158e0
Marek Lipert <marek@spinney.io>
2024-12-22 15:40:45 +0100

    Add failing tests.

---
+++ src/invaders/parser.clj
@@ -0,0 +1,6 @@
+(ns invaders.parser)
+
+(defn parse-radar [filename])
+
+
+
--- test/invaders/core_test.clj
+++ test/invaders/core_test.clj
@@ -1,7 +1,37 @@
 (ns invaders.core-test
   (:require [clojure.test :refer :all]
-            [invaders.core :refer :all]))
+            [invaders.parser :as parser]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def pattern-1 [[0 0 1 0 0 0 0 0 1 0 0]
+                [0 0 0 1 0 0 0 1 0 0 0]
+                [0 0 1 1 1 1 1 1 1 0 0]
+                [0 1 1 0 1 1 1 0 1 1 0]
+                [1 1 1 1 1 1 1 1 1 1 1]
+                [1 0 1 1 1 1 1 1 1 0 1]
+                [1 0 1 0 0 0 0 0 1 0 1]
+                [0 0 0 1 1 0 1 1 0 0 0]])
+
+(def pattern-2 [[0 0 0 1 1 0 0 0]
+                [0 0 1 1 1 1 0 0]
+                [0 1 1 1 1 1 1 0]
+                [1 1 0 1 1 0 1 1]
+                [1 1 1 1 1 1 1 1]
+                [0 0 1 0 0 1 0 0]
+                [0 1 0 1 1 0 1 0]
+                [1 0 1 0 0 1 0 1]])
+
+(deftest test-patterns-parsing
+  (testing "Pattern 1"
+    (is (= pattern-1 (parser/parse-radar "resources/invader-1.txt")))
+    (is (not= pattern-2 (parser/parse-radar "resources/invader-1.txt"))))
+  (testing "Pattern 2"
+    (is (= pattern-2 (parser/parse-radar "resources/invader-2.txt")))
+    (is (not= pattern-1 (parser/parse-radar "resources/invader-2.txt"))))  
+  (testing "Sample 1"
+    (is (= 0 (ffirst (parser/parse-radar "resources/sample-1.txt"))))
+    (is (= 50 (count (parser/parse-radar "resources/sample-1.txt"))))
+    (is (= 100 (count (first (parser/parse-radar "resources/sample-1.txt")))))
+    (is (= 1 (-> (parser/parse-radar "resources/sample-1.txt")
+                 (nth 5)
+                 (nth 10))))    
+    (is (not= pattern-1 (parser/parse-radar "resources/sample-1.txt")))))
