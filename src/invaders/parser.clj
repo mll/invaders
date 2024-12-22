503d5a4a1a8149e96f9f3ecfa1ddb1b0b10c7ba6
Marek Lipert <marek@spinney.io>
2024-12-22 15:41:20 +0100

    Add implementation.

--- src/invaders/parser.clj
+++ src/invaders/parser.clj
@@ -1,6 +1,17 @@
-(ns invaders.parser)
+(ns invaders.parser
+  (:require [invaders.schema :as schema]
+            [clojure.spec.alpha :as spec]
+            [clojure.string :refer [split-lines]]))
 
-(defn parse-radar [filename])
+(defn parse-radar [filename]
+  {:pre [(spec/valid? string? filename)]
+   :post [(spec/valid? :invaders.schema/radar-pattern %)]}
+  (let [lines (-> filename slurp split-lines)
+        char->digit (fn [ch] (if (= ch \-) 0 1))]
+    (mapv
+     (fn [line]
+       (mapv char->digit line))
+     lines)))
 
 
 
