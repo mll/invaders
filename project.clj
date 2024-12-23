dfcbb503328221a6b5616f84a5cfddda82403207
Marek Lipert <marek@spinney.io>
2024-12-23 12:55:33 +0100

    Cleanup default project files.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject invaders "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject invaders "1.0.0"
+  :description "Detect space invaders"
+  :url "https://github.com/mll"
   :main invaders.core/-main
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
